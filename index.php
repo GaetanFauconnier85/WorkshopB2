d6c6d366aa4fbd5e27532da2a196fca688823744
Fauconnier Gaëtan <fauconnier85@gmail.com>
2018-09-11 17:04:13 +0200

    oublie

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
 
 inscription a rajouter
-<a href="Acceuill.php">Acceuill</a>
+<a href="Acceuil.php">Acceuill</a>
 
         <link href="Acceuilcss.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
